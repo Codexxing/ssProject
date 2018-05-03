a9614cb287cd26b2c5349382683f24c756c3bf14
ly671205@163.com <ly671205@163.com>
2018-05-03 16:04:27 +0800

    redis

--- application/jdroom/controller/Index.php
+++ application/jdroom/controller/Index.php
@@ -23,7 +23,12 @@ class Index extends AdminBase
      */
     public function index()
     {
-        phpinfo();die;
+        $redis = new Redis();
+             //连接
+     $redis->connect('127.0.0.1', 6379);
+        var_dump($redis);
+     //检测是否连接成功
+     echo "Server is running: " . $redis->ping();die;
 //        $param['order_number']='SH2018042110255101';
 //        $conten =  Db::table('os_order_list')
 //            ->alias('list')
